c5512e002278f48fb8ea80a10d2ad618c9f36d54
dark231321 <89053010376Dan>
2021-03-06 16:45:54 +0400

    cm

--- src/com/company/lab0/TriangularMatrix.java
+++ src/com/company/lab0/TriangularMatrix.java
@@ -9,37 +9,35 @@ import java.util.Scanner;
 
 public class TriangularMatrix {
 
-
-    private double[][] parseMatrixFromFile(String fileName){
-        File file = new File(fileName);
+    private double[][] parseMatrixFromFile(){
+        File file = new File(FilePaths.pathToTriangularMatrixFile);
         double[][] value = null;
         try {
             Scanner scanner = new Scanner(file);
             String[] temp = scanner.nextLine().split(" ");
-            int nMatrix = Integer.parseInt(temp[0]);
-            int m = nMatrix + 1;
+            int n = Integer.parseInt(temp[0]);
+            int m = n + 1;
 
-            value = new double[nMatrix + 1][m + 1];
-            for (int i = 1; i <= nMatrix; i++) {
+            value = new double[n + 1][m + 1];
+            for (int i = 1; i <= n; i++) {
                 String[] numbers = scanner.nextLine().split(" ");
                 for (int j = 1; j <= m; j++) {
                     value[i][j] = Double.parseDouble(numbers[j - 1]);
                 }
             }
             scanner.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e){
             e.printStackTrace();
         }
         return value;
     }
 
-    private double[] method(int n, double[][] A){
-        int m = n + 1;
+    private double[] methodHigher(int n, double[][] A){
         double[] x = new double[n + 1];
 
-        x[n] = A[n][m]/A[n][n];
+        x[n] = A[n][n + 1]/A[n][n];
         for (int i = n - 1; i >= 1; i--) {
-            x[i] = A[i][m];
+            x[i] = A[i][n + 1];
             for (int j = i + 1; j <= n; j++)
                 x[i] -= A[i][j] * x[j];
             x[i] = x[i]/A[i][i];
@@ -47,11 +45,25 @@ public class TriangularMatrix {
         return x;
     }
 
+    private double[] methodLower(int n, double[][] A){
+        double[] x = new double[n + 1];
+
+        x[1] = A[1][n + 1]/A[1][1];
+        for (int i = 2; i <= n; i++) {
+            x[i] = A[i][n + 1];
+            for (int j = i - 1; j >= 1; j--)
+                x[i] -= A[i][j] * x[j];
+            x[i] = x[i]/A[i][i];
+        }
+        return x;
+    }
 
     public static void main(String[] args) {
         TriangularMatrix triangularMatrix = new TriangularMatrix();
-        double[][] A = triangularMatrix.parseMatrixFromFile(FilePaths.pathToTriangularMatrixFile);
+        double[][] A = triangularMatrix.parseMatrixFromFile();
+        System.out.println("Input matrix:");
         Arrays.stream(A).forEach(doubles -> System.out.println(Arrays.toString(doubles)));
-        System.out.println(Arrays.toString(triangularMatrix.method(A.length - 1, A)));
+        System.out.println("Method result:");
+        System.out.println(Arrays.toString(triangularMatrix.methodLower(A.length - 1, A)));
     }
 }
